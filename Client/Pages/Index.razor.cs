3110c5fd9de2fdd0b5131d7089c0c6bbf2315daa
Kyle Andrews <codingwithkyle@gmail.com>
2020-08-27 21:10:46 -0400

    Converted Index to code-behind pattern

---
+++ Client/Pages/Index.razor.cs
@@ -0,0 +1,95 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.JSInterop;
+using FreeTabletop.Client.Models;
+
+namespace FreeTabletop.Client
+{
+    public partial class IndexBase : ComponentBase
+    {
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+
+        public JoinRoomForm room = new JoinRoomForm();
+        public NewPlayerForm player = new NewPlayerForm();
+
+        public string errorMessage = null;
+        public string view = "initial";
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (!Networker.IsConnected)
+            {
+                await Networker.Connect(NavigationManager.ToAbsoluteUri("/gamehub"));
+            }
+
+            if (Networker.IsConnected)
+            {
+                Networker.hubConnection.On<string, string>("Load:GM", GoToRoom);
+                Networker.hubConnection.On<string, string>("Load:Player", GoToRoom);
+
+                Networker.hubConnection.On("Get:PlayerName", () =>
+                {
+                    view = "player";
+                    StateHasChanged();
+                    FocusElement("#name");
+                });
+
+                Networker.hubConnection.On("Error:RoomNotFound", () =>
+                {
+                    errorMessage = "Room not found";
+                    room.RoomCode = "";
+                    StateHasChanged();
+                    FocusElement("#okay-button");
+                });
+
+                Networker.hubConnection.On("Error:RoomIsLocked", () =>
+                {
+                    errorMessage = "Room " + room.RoomCode.ToUpper() + " is locked";
+                    StateHasChanged();
+                    FocusElement("#okay-button");
+                });
+            }
+
+            FocusElement("#roomCode");
+        }
+
+        private async Task FocusElement(string selector)
+        {
+            await JSRuntime.InvokeVoidAsync("FocusElement", selector);
+        }
+
+        private async Task GoToRoom(string roomCode, string uid)
+        {
+            await JSRuntime.InvokeVoidAsync("SetPlayerUID", uid);
+            NavigationManager.NavigateTo("room/" + roomCode.ToLower());
+        }
+
+        public async Task HandleSubmitName()
+        {
+            await Networker.hubConnection.SendAsync("Player:JoinRoom", room.RoomCode, player.Name);
+        }
+
+        public async Task HandleCreateRoom()
+        {
+            await Networker.hubConnection.SendAsync("Create:Room");
+        }
+
+        public async Task HandleSubmitRoomCode()
+        {
+            string savedUID = await JSRuntime.InvokeAsync<string>("GetPlayerUID");
+            await Networker.hubConnection.SendAsync("Player:LookupRoom", room.RoomCode, savedUID);
+        }
+
+        public void ClearMessage()
+        {
+            errorMessage = null;
+            StateHasChanged();
+        }
+    }
+}
