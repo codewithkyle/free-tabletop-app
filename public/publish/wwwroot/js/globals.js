8774a5f1103b1f8eb469ea1d6a0a92ed7b35d064
Kyle Andrews <codingwithkyle@gmail.com>
2020-11-01 12:19:55 -0500

    Added public release build

---
+++ public/publish/wwwroot/js/globals.js
@@ -0,0 +1,349 @@
+function SetPlayerUID(uid) {
+    localStorage.setItem("PlayerUID", uid);
+}
+function GetPlayerUID() {
+    return localStorage.getItem("PlayerUID");
+}
+function ClearStorage() {
+    localStorage.clear();
+    sessionStorage.clear();
+}
+function CopyToClipboard(value) {
+    if ("clipboard" in navigator) {
+        navigator.clipboard.writeText(value);
+    }
+}
+function ForceHome() {
+    location.href = location.origin;
+}
+function FocusElement(selector) {
+    const el = document.body.querySelector(selector);
+    if (el) {
+        el.focus();
+    }
+}
+function Debug(thing) {
+    console.log(thing);
+}
+async function GetGridSize(url) {
+    let grid = [0, 0];
+    if (url.length) {
+        grid = await new Promise((resolve) => {
+            const tempImg = document.createElement("img");
+            tempImg.src = url;
+            tempImg.className = "temp-image";
+            tempImg.addEventListener("load", () => {
+                const bounds = tempImg.getBoundingClientRect();
+                const width = Math.floor(bounds.width / 32);
+                const height = Math.floor(bounds.height / 32);
+                resolve([width, height]);
+            });
+            tempImg.addEventListener("error", () => {
+                resolve([0, 0]);
+            });
+            document.body.appendChild(tempImg);
+        });
+    }
+    return grid;
+}
+document.onkeydown = (event) => {
+    const key = event.key.toLowerCase();
+    if (key === "f5") {
+        event.returnValue = false;
+        return false;
+    }
+    else if (key === "r" && (event.ctrlKey || event.metaKey)) {
+        event.returnValue = false;
+        return false;
+    }
+};
+function ClearHighlightedCells() {
+    const cells = Array.from(document.body.querySelectorAll("td.highlight"));
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].className = "";
+    }
+}
+function uid() {
+    return new Array(4)
+        .fill(0)
+        .map(() => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16))
+        .join("-");
+}
+// DB Worker
+let dbWorker = null;
+let lastDBWorkerUid = null;
+function SyncMonsterData() {
+    if (!dbWorker) {
+        console.log("Attempting to load db worker");
+        dbWorker = new Worker(`/js/db-worker.js`);
+    }
+}
+function LookupCreature(query) {
+    return new Promise((resolve) => {
+        if (!dbWorker) {
+            resolve([]);
+        }
+        lastDBWorkerUid = uid();
+        dbWorker.onmessage = (e) => {
+            const data = e.data;
+            if (data.messageUid === lastDBWorkerUid) {
+                const creature = { ...data.creature };
+                creature.BaseName = toUpper(creature.BaseName);
+                resolve(JSON.stringify(creature));
+            }
+            else {
+                resolve(JSON.stringify([]));
+            }
+        };
+        dbWorker.postMessage({
+            type: "lookup",
+            query: query,
+            messageUid: lastDBWorkerUid,
+        });
+    });
+}
+function AddCustomCreature(creature) {
+    if (!dbWorker) {
+        return;
+    }
+    dbWorker.postMessage({
+        type: "add",
+        creature: creature,
+    });
+}
+function GetCreatures() {
+    return new Promise((resolve) => {
+        if (!dbWorker) {
+            resolve([]);
+        }
+        lastDBWorkerUid = uid();
+        dbWorker.onmessage = (e) => {
+            const data = e.data;
+            if (data.messageUid === lastDBWorkerUid) {
+                resolve(JSON.stringify(data.creatures));
+            }
+            else {
+                resolve(JSON.stringify([]));
+            }
+        };
+        dbWorker.postMessage({
+            type: "get",
+            messageUid: lastDBWorkerUid,
+        });
+    });
+}
+// Notifications
+function PlayerConnected(name) {
+    toast({
+        title: `${name} Joined`,
+        message: `${name} has joined to the room.`,
+        duration: 5,
+        classes: "-green",
+    });
+    PlaySound("alert.wav");
+}
+function PlayerDisconnected(name) {
+    toast({
+        title: `${name} Disconnected`,
+        message: `${name} has disconnected from the room.`,
+        duration: 5,
+        classes: "-red",
+    });
+}
+function PlayerReconnected(name) {
+    toast({
+        title: `${name} Reconnected`,
+        message: `${name} has reconnected to the room.`,
+        duration: 5,
+        classes: "-green",
+    });
+}
+function PlayerKicked(name) {
+    toast({
+        title: `${name} Disconnected`,
+        message: `${name} was kicked from the room.`,
+        duration: 5,
+        classes: "-red",
+    });
+}
+function TakeTurn() {
+    toast({
+        title: `You're Up`,
+        message: `It's your turn for combat, use it wisely.`,
+        duration: 5,
+    });
+    PlaySound("alert.wav");
+}
+function OnDeck() {
+    toast({
+        title: `On Deck`,
+        message: `You're up next for combat. Start planning your turn.`,
+        duration: 5,
+    });
+}
+function toUpper(str) {
+    return str
+        .toLowerCase()
+        .split(" ")
+        .map(function (word) {
+        return word[0].toUpperCase() + word.substr(1);
+    })
+        .join(" ");
+}
+function EntityOnDeck(name) {
+    const fixedName = toUpper(name);
+    toast({
+        title: `${fixedName} Is On Deck`,
+        message: `${fixedName} is up next for combat.`,
+        duration: 5,
+    });
+}
+function PlaySound(name) {
+    var audio = new Audio(`${location.origin}/sfx/${name}`);
+    audio.play();
+}
+function Ping(x, y) {
+    var audio = new Audio(`${location.origin}/sfx/ping.mp3`);
+    audio.volume = 0.75;
+    audio.play();
+    const el = document.createElement("div");
+    el.className = "ping";
+    el.style.cssText = `top:${y - 24}px;left:${x - 24}px;`;
+    el.innerHTML = `
+        <i>
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info" class="svg-inline--fa fa-info fa-w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path fill="currentColor" d="M20 424.229h20V279.771H20c-11.046 0-20-8.954-20-20V212c0-11.046 8.954-20 20-20h112c11.046 0 20 8.954 20 20v212.229h20c11.046 0 20 8.954 20 20V492c0 11.046-8.954 20-20 20H20c-11.046 0-20-8.954-20-20v-47.771c0-11.046 8.954-20 20-20zM96 0C56.235 0 24 32.235 24 72s32.235 72 72 72 72-32.235 72-72S135.764 0 96 0z"></path></svg>
+        </i>
+    `;
+    document.body.appendChild(el);
+    setTimeout(() => {
+        el.remove();
+    }, 2000);
+}
+// Combat window
+function StartCombatDrag() {
+    const modal = document.body.querySelector(".combat-modal");
+    const el = document.body.querySelector(".js-combat-modal");
+    let dragging = false;
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    el.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        dragging = true;
+    });
+    document.addEventListener("mouseup", (e) => {
+        e.preventDefault();
+        dragging = false;
+    });
+    document.addEventListener("mousemove", (e) => {
+        if (dragging) {
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            modal.style.top = (modal.offsetTop - pos2) + "px";
+            modal.style.left = (modal.offsetLeft - pos1) + "px";
+        }
+    });
+}
+function ResetCombatModal() {
+    const modal = document.body.querySelector(".combat-modal");
+    modal.style.left = '0px';
+    modal.style.top = "calc(36px + 0.5rem)";
+}
+// Messenger window
+function StartChatDrag() {
+    const modal = document.body.querySelector(".chat-modal");
+    const el = document.body.querySelector(".js-chat-modal");
+    let dragging = false;
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    el.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        dragging = true;
+    });
+    document.addEventListener("mouseup", (e) => {
+        e.preventDefault();
+        dragging = false;
+    });
+    document.addEventListener("mousemove", (e) => {
+        if (dragging) {
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            modal.style.top = (modal.offsetTop - pos2) + "px";
+            modal.style.left = (modal.offsetLeft - pos1) + "px";
+        }
+    });
+}
+function ResetChatModal() {
+    const modal = document.body.querySelector(".chat-modal");
+    modal.style.left = '0px';
+    modal.style.top = "calc(36px + 0.5rem)";
+}
+function ResetChatMessage() {
+    const textarea = document.body.querySelector(".js-messenger-input");
+    textarea.value = "";
+    textarea.innerHTML = "";
+    textarea.style.height = `0px`;
+}
+function GetChatMessage() {
+    const textarea = document.body.querySelector(".js-messenger-input");
+    return textarea.value;
+}
+function AdjustChatMessageHeight() {
+    const textarea = document.body.querySelector(".js-messenger-input");
+    textarea.style.height = `${textarea.scrollHeight}px`;
+}
+function ScrollChatMessages() {
+    const container = document.body.querySelector(".js-chat-messages");
+    if (container) {
+        container.scrollTo({
+            top: container.scrollHeight,
+            left: 0,
+            behavior: "auto",
+        });
+    }
+}
+function DragTabletop() {
+    const tabletop = document.body.querySelector(".js-tabletop");
+    if (tabletop) {
+        let pos = { top: 0, left: 0, x: 0, y: 0 };
+        let movingTabletop = false;
+        tabletop.addEventListener("mousedown", (e) => {
+            if (e.button === 0 && e.target instanceof HTMLTableCellElement) {
+                pos = {
+                    left: tabletop.scrollLeft,
+                    top: tabletop.scrollTop,
+                    x: e.clientX,
+                    y: e.clientY,
+                };
+                movingTabletop = true;
+            }
+        });
+        document.addEventListener('mousemove', (e) => {
+            if (movingTabletop) {
+                const dx = e.clientX - pos.x;
+                const dy = e.clientY - pos.y;
+                tabletop.scrollTo({
+                    top: pos.top - dy,
+                    left: pos.left - dx,
+                    behavior: "auto",
+                });
+            }
+        });
+        document.addEventListener('mouseup', (e) => {
+            movingTabletop = false;
+        });
+    }
+}
---
+++ public/publish/wwwroot/service-worker-assets.js
@@ -0,0 +1,385 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-eKDZsQzhfvTisqjar0XRK3PX4g3MfLKzT47odXqn5hc=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-E3k5zz3gok+ifulPfwpZIsh1oTGBJJc5ggP7YuAs7e8=",
+      "url": "css\/brixi.css"
+    },
+    {
+      "hash": "sha256-HSnxmsQmuIAavjJzSTK4XA8dW7VnTTRT1q7+a1+YDis=",
+      "url": "css\/buttons.css"
+    },
+    {
+      "hash": "sha256-oK0i25+YuvTrePhjQAnTzdhw3VOIZnpS6bVC9gE+c8M=",
+      "url": "css\/form.css"
+    },
+    {
+      "hash": "sha256-aXIB7\/kG7L1C64dcrveewGZKBz9wY1cbRV7JOvd+E0c=",
+      "url": "css\/game.css"
+    },
+    {
+      "hash": "sha256-sTX0xm0142nNOPXAIhXt3rjJ7aAmRSD1v9xH+M1FDSU=",
+      "url": "css\/input.css"
+    },
+    {
+      "hash": "sha256-7BF4lc46UCSEJq\/hRNllSUdOlK\/iq6Neh5plzBtGy5g=",
+      "url": "css\/lightswitch.css"
+    },
+    {
+      "hash": "sha256-CnChzBrOFlQ1BJT+e3DQuven4I+CEBcFq86zaorN4CI=",
+      "url": "css\/normalize.css"
+    },
+    {
+      "hash": "sha256-9vub\/Ova2RPe+8L7zqJbGxWDt+OduFiDVIoDcOCBkE8=",
+      "url": "css\/select.css"
+    },
+    {
+      "hash": "sha256-5o5VF1gLkCZaGgjG3IsJEjwmNE5lOAeK3PZmiO4a490=",
+      "url": "css\/tabletop.css"
+    },
+    {
+      "hash": "sha256-8YEWLaik+IrnUQte4WgoNoxrSrF2X8T4YMtV7+4KeUs=",
+      "url": "css\/toast.css"
+    },
+    {
+      "hash": "sha256-IpHFcQvs03lh6W54zoeJRG1jW+VnGmwymzxLoXtKX7Y=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-af2ggFldh2zHjxoxCImeR3vCkNtzkaJtMLySQQgViYY=",
+      "url": "icons\/icon-120x120.png"
+    },
+    {
+      "hash": "sha256-TcAsZzaq3c07d5swQRZaci5+d4eJq2\/hM29vV3NnYRk=",
+      "url": "icons\/icon-128x128.png"
+    },
+    {
+      "hash": "sha256-QubdV2i3ah7U3cflsQ63ez\/51WaDJ4hAYPOZlrbdLZs=",
+      "url": "icons\/icon-144x144.png"
+    },
+    {
+      "hash": "sha256-mFwf2efWToW4EPMoS7BgLDS5v98YXZ0fKobHbGh6Xn8=",
+      "url": "icons\/icon-152x152.png"
+    },
+    {
+      "hash": "sha256-DjNJjfm\/s\/1sfAQQzNRZ6ugC28D4WG+dqSHSqR+OVIE=",
+      "url": "icons\/icon-167x167.png"
+    },
+    {
+      "hash": "sha256-yA2Wm+w6lEUsnPdtN2tGhqXKMvsmBVY\/HmPtcAexhb8=",
+      "url": "icons\/icon-16x16.png"
+    },
+    {
+      "hash": "sha256-AU\/QO+eRU7SyLzQV37hO\/8u9S0Lnee1QSM3JltiIGEA=",
+      "url": "icons\/icon-180x180.png"
+    },
+    {
+      "hash": "sha256-AJz7XROidJtYxN2e95GKGkYxeW3hCbKNIrQrwYatXnc=",
+      "url": "icons\/icon-192x192.png"
+    },
+    {
+      "hash": "sha256-Ls+w7iZb5DYIsMBWV9Z61u0GY2nwl8M4yGj076BW6FI=",
+      "url": "icons\/icon-256x256.png"
+    },
+    {
+      "hash": "sha256-vzIs3v9TFU2hLz2K8zeAnnM67qkkIft8WG\/crPwRwzw=",
+      "url": "icons\/icon-32x32.png"
+    },
+    {
+      "hash": "sha256-TSpz7u0BAb8ACwkxQfQWw87VCdDeQwy6q4iegscb4+k=",
+      "url": "icons\/icon-512x512.png"
+    },
+    {
+      "hash": "sha256-n\/YdslL0J2YBfJy9fCdgq4OJLP3HuopfYW84XdjMr2Y=",
+      "url": "icons\/icon-96x96.png"
+    },
+    {
+      "hash": "sha256-0cjfDWtI14iSTLCgQS8+ZIijuj69chehVVfMZsUyt2o=",
+      "url": "icons\/maskable\/icon-128x128.png"
+    },
+    {
+      "hash": "sha256-t7q0iqnK5pusJHvwQy4M1h+\/WvvYsdap+qFjEqgyRyY=",
+      "url": "icons\/maskable\/icon-144x144.png"
+    },
+    {
+      "hash": "sha256-96CletG1HnyALIOdm2R7tJDdsD04fZMy92k6VgIp5WU=",
+      "url": "icons\/maskable\/icon-152x152.png"
+    },
+    {
+      "hash": "sha256-zgbo3gpMR9r4rpXpay6s1l47fUFU7MyEdN0PceJDwy0=",
+      "url": "icons\/maskable\/icon-192x192.png"
+    },
+    {
+      "hash": "sha256-Fjqg3hpOVd4LErHd7DxCG8xAuguU0cUuMqckF0A3nVY=",
+      "url": "icons\/maskable\/icon-256x256.png"
+    },
+    {
+      "hash": "sha256-K+TNeSzdQw+HH+hN1B5A+XrTAeQ8ck8VBQ+7OWBvWK4=",
+      "url": "icons\/maskable\/icon-512x512.png"
+    },
+    {
+      "hash": "sha256-i9LGDOJRQ\/Hm2lWtBZ81xqgDX8HGBztIpnlhXthHdsg=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-9FK+MQ1nmV7E2Dv46bFM0ySGj0RRl08LYAwHVhGfdlo=",
+      "url": "js\/db-worker.js"
+    },
+    {
+      "hash": "sha256-YhkOfm2Rblq6D\/QfSXa0SnIBLaohC6inKRyi3hLHq6E=",
+      "url": "js\/dice.js"
+    },
+    {
+      "hash": "sha256-q9iDVK0TXhoIZkS4OXEDwqD8gFFhXbwXxBFUIfDdq5A=",
+      "url": "js\/globals.js"
+    },
+    {
+      "hash": "sha256-CtR5k+hjzoEZPoJhnkjN90sfpDUAokhhFdN3wQ3Rh8k=",
+      "url": "js\/notify.js"
+    },
+    {
+      "hash": "sha256-h7l+JaPzcmIRdo1YlROuYC1wQh1BZ\/6DSlsbHncdmkQ=",
+      "url": "manifest.json"
+    },
+    {
+      "hash": "sha256-KPrV4IZfzlNQpR9+NGk7RrFQmipUScTnWUcTrNjRDR8=",
+      "url": "service-worker.killer.js"
+    },
+    {
+      "hash": "sha256-tBT4mHTPJ2HOT5KDWFCAaiZvcrEbwItLNw4dogLcdyM=",
+      "url": "sfx\/alert.wav"
+    },
+    {
+      "hash": "sha256-6dXbFgLe1mqvRz1bDpOG0s6iNf08wjQPvlAhnzjE2eM=",
+      "url": "sfx\/celebration.wav"
+    },
+    {
+      "hash": "sha256-kb5Xnvsbu8TOHtnzrIL3o183uxu1Dvjpa4mcMbRXfao=",
+      "url": "sfx\/loading.wav"
+    },
+    {
+      "hash": "sha256-62tX860uhWqb2nsL2qtbJCenusHu7SoPOSjR6Yi\/F8A=",
+      "url": "sfx\/message.wav"
+    },
+    {
+      "hash": "sha256-2E6Y+zvQW8f1JZXpqiG2APnjDFhHA5IdSF3T\/aXLdXQ=",
+      "url": "sfx\/ping.mp3"
+    },
+    {
+      "hash": "sha256-8PAPusTTUlg0xlarb84WsPXeVu15GOI3CAcKedYvaD0=",
+      "url": "sfx\/plop.wav"
+    },
+    {
+      "hash": "sha256-PLzG6CevPdkGe9JKX1mGtn8XdO6SAfJvplMV4GIno88=",
+      "url": "sfx\/success.wav"
+    },
+    {
+      "hash": "sha256-Vm8DQCF3OzJuKLmTbGSZcR\/1uKBmnZIqKTEUDw7JCwA=",
+      "url": "_framework\/_bin\/FreeTabletop.Client.dll"
+    },
+    {
+      "hash": "sha256-63Neea+hwIeZGAMikhMskSYQeg+JsjKi15UYKfmg\/bs=",
+      "url": "_framework\/_bin\/FreeTabletop.Shared.dll"
+    },
+    {
+      "hash": "sha256-lrNhhkvCBsbDSQNTk0Zmqg10zWoq7CIgrAjwaVoHpR0=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.dll"
+    },
+    {
+      "hash": "sha256-dAlX5I5YA8vaHAXhaRT+NabpcG0Fj5AF+EFuobLsif0=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.Forms.dll"
+    },
+    {
+      "hash": "sha256-39AjqAw67IjokIHYUKGeI7AD7bAljdu0rktcHjVLe00=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.Web.dll"
+    },
+    {
+      "hash": "sha256-Jj6rXkGdmw+2+Kr+mBtW7p70pv32gUrErp\/H15ljaW0=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-PtD2b5olua4w8ICczwXkKA3dwyUH3Qt0ROs3wPH+ZzI=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Connections.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-KfIshtu4kyzwunuYRJAYt22UoqZDCDM44ZHT3aZfoVI=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Http.Connections.Client.dll"
+    },
+    {
+      "hash": "sha256-ubfTCG3I8XtfUHhGBdbj7XpmzSHVk5PT0g7R6qPE5qs=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Http.Connections.Common.dll"
+    },
+    {
+      "hash": "sha256-LBMjWxGQOicjFnvrsC\/Z0qMrf3CgJfJeBisD93Un8CI=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Http.Features.dll"
+    },
+    {
+      "hash": "sha256-vsdTTKw+cIfE9wUObZj+ED4QNROz+kByABRnE5FWego=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.SignalR.Client.Core.dll"
+    },
+    {
+      "hash": "sha256-4v+gv5CkRXYD0VklGLXT5eENU+EpaRV2Zj78l3ADZb4=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.SignalR.Client.dll"
+    },
+    {
+      "hash": "sha256-S5CPJTNB6Bo6TrtQPBhRJSutemnOgmU45XofGUpIsIo=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.SignalR.Common.dll"
+    },
+    {
+      "hash": "sha256-diq2G2PAWdjdNPz87dUzafERPvKAxPM7D90XfJZbZCs=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.SignalR.Protocols.Json.dll"
+    },
+    {
+      "hash": "sha256-jzHgXWAvWMkKIGFjZoT84tbe72E+H7CvTr\/Dryh4QPs=",
+      "url": "_framework\/_bin\/Microsoft.Bcl.AsyncInterfaces.dll"
+    },
+    {
+      "hash": "sha256-cOZaiYjpSt35Mpw4IC26nTyE\/FOa7azYfdlw3F2bpp4=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-ZsdukLsYkEVXFXQREzE5V+ohOiEPcScHt3c\/BsTDgxE=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.dll"
+    },
+    {
+      "hash": "sha256-hCUxVHGQslba\/E4SaIQ22suJsjrd+MR7HYYIc0ixyvA=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Json.dll"
+    },
+    {
+      "hash": "sha256-HzDa5nd1Z8KiqLg1XarLDlAm\/e8BVRtZt64mysvW4lA=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-PIh5DJBPTQhT7OuvpqEZ0LtF6EPH1iURqYf8gpAFmic=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.dll"
+    },
+    {
+      "hash": "sha256-ntCosFV0y4897ag2gykpnO3tnESNIaVt8DpV6K1VCyg=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Logging.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-p9TyDpo6RaywD2N9bjY4p2bj5y9NdqjSYbh3X9Yr1RI=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Logging.dll"
+    },
+    {
+      "hash": "sha256-IvHX6uk2Vwkfo+q4HZK5sY7A+HK99PXfU1MArl4Kdf4=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Options.dll"
+    },
+    {
+      "hash": "sha256-gQ6NMXIERv6yZjR044uooiALFHcjjJj2Eq3Xp7QGYJk=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Primitives.dll"
+    },
+    {
+      "hash": "sha256-3cbvJEo8k8VJVQwyEZWWQ6yGRFQW8\/219DaOnmsX3e4=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.dll"
+    },
+    {
+      "hash": "sha256-zV\/OfJNfNsG404X30Pwkdymdh1KmZpTVEmG\/JNFVc\/k=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-qYkPY\/riHpDWOurU9R8Iu4GJn7UrIvE3DPg+FIw7uhE=",
+      "url": "_framework\/_bin\/Mono.Security.dll"
+    },
+    {
+      "hash": "sha256-IHnytmyqXjZe7C7B5uhn0nNq0k\/ODjPUX\/mwiIgecpw=",
+      "url": "_framework\/_bin\/mscorlib.dll"
+    },
+    {
+      "hash": "sha256-8+aWlQshkso8ei8xp5J0gn4ZNGRYRMqXzI+osKzgVYI=",
+      "url": "_framework\/_bin\/Newtonsoft.Json.dll"
+    },
+    {
+      "hash": "sha256-uT3EU60ELPr0hg8NjxL59MHHnh9EsWbeROfi+K0ENJk=",
+      "url": "_framework\/_bin\/System.ComponentModel.DataAnnotations.dll"
+    },
+    {
+      "hash": "sha256-rFRi034P+GBbBEZiDmZDGr+cCc\/fhWumxhA6DA5thww=",
+      "url": "_framework\/_bin\/System.Core.dll"
+    },
+    {
+      "hash": "sha256-+VUCzn8BkaztWuijtQXzFAqFp7SJrf4dfj2rxaFxCQQ=",
+      "url": "_framework\/_bin\/System.Data.dll"
+    },
+    {
+      "hash": "sha256-kpb3z9TBru3aC8+jPl+oS6MZ+Shw\/SQHi1Dekwrcmbw=",
+      "url": "_framework\/_bin\/System.dll"
+    },
+    {
+      "hash": "sha256-AwknkoTGBCLi0GkyE9+pVHAKwFHm15LvRMI26rgF3d8=",
+      "url": "_framework\/_bin\/System.IO.Pipelines.dll"
+    },
+    {
+      "hash": "sha256-hpr8IcDG1skHa8+i1SWqZBOJD4VsDxrj3JByUXfrCu4=",
+      "url": "_framework\/_bin\/System.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-2Twp0kdAw\/SA9t006vvIEcS\/KgP6AziyNyR0VnBq+Zc=",
+      "url": "_framework\/_bin\/System.Net.Http.WebAssemblyHttpHandler.dll"
+    },
+    {
+      "hash": "sha256-9Jmm8s0Tz8kEOpoBs0qn1b8O+E9YqPtH+TT8hUPYPrE=",
+      "url": "_framework\/_bin\/System.Numerics.dll"
+    },
+    {
+      "hash": "sha256-zdPPGF3erJcm0JXbBYuhRvTGVQvrGaGhdmSQ0TQCS+Q=",
+      "url": "_framework\/_bin\/System.Runtime.CompilerServices.Unsafe.dll"
+    },
+    {
+      "hash": "sha256-DCQekEcRwvGFLTXROJnsMr\/hMDHG5YIY4CGv514BuI0=",
+      "url": "_framework\/_bin\/System.Runtime.Serialization.dll"
+    },
+    {
+      "hash": "sha256-8ns57AfZm9D3XcNgb0jMRfYeOcjeqNDa+G8DJReyMNk=",
+      "url": "_framework\/_bin\/System.Text.Encodings.Web.dll"
+    },
+    {
+      "hash": "sha256-Tq9frmopD2OBorXdgP54nHE72gaJLUXUbTKHdJahdv8=",
+      "url": "_framework\/_bin\/System.Text.Json.dll"
+    },
+    {
+      "hash": "sha256-1gF3xZM9NhqLFsypCtUEQUjaAyQI3J\/ATiiOw2PzTY4=",
+      "url": "_framework\/_bin\/System.Threading.Channels.dll"
+    },
+    {
+      "hash": "sha256-trgE00bhS7jSiTtun+2jhJhIsfE3Oxta0LpHOtsxrHw=",
+      "url": "_framework\/_bin\/System.Xml.dll"
+    },
+    {
+      "hash": "sha256-DUTERB+OHUirW\/0URIYM8eO+ZbfZ5\/B7ay3zWN2jGRU=",
+      "url": "_framework\/_bin\/System.Xml.Linq.dll"
+    },
+    {
+      "hash": "sha256-KUUqCX+YIYgEvhPplWEXaVpEU4M5WMTqEG2aa3bj\/24=",
+      "url": "_framework\/_bin\/WebAssembly.Bindings.dll"
+    },
+    {
+      "hash": "sha256-agzg\/SGT74f\/cr2CrFw4mQz8\/I8gyYoiLrmzM3YN6Bg=",
+      "url": "_framework\/_bin\/WebAssembly.Net.WebSockets.dll"
+    },
+    {
+      "hash": "sha256-mPoqx7XczFHBWk3gRNn0hc9ekG1OvkKY4XiKRY5Mj5U=",
+      "url": "_framework\/wasm\/dotnet.3.2.0.js"
+    },
+    {
+      "hash": "sha256-3S0qzYaBEKOBXarzVLNzNAFXlwJr6nI3lFlYUpQTPH8=",
+      "url": "_framework\/wasm\/dotnet.timezones.dat"
+    },
+    {
+      "hash": "sha256-UC\/3Rm1NkdNdlIrzYARo+dO\/HDlS5mhPxo0IQv7kma8=",
+      "url": "_framework\/wasm\/dotnet.wasm"
+    },
+    {
+      "hash": "sha256-SPHS1EAPAcMFN4TEIUYl3FWtoCQTsNJch0XVGgok1eE=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-ZHLUM4C8tt020nXlyPHrXI49YWbIcLuVGb8bEpr0S9o=",
+      "url": "_framework\/blazor.boot.json"
+    }
+  ],
+  "version": "SOcMhy5c"
+};
