7ca1b79c8a91940094701e7f289d1925c0f86c8d
Kyle Andrews <codingwithkyle@gmail.com>
2020-11-06 15:09:35 -0500

    Updating CI pipeline

--- build/release.js
+++ build/release.js
@@ -1,25 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 const cwd = process.cwd();
-const glob = require("glob");
 
 const project = require(path.join(cwd, "package.json"));
 const publicDir = path.join(cwd, "Client", "wwwroot");
-const data = {};
 
 if (fs.existsSync(path.join(publicDir, "app.json"))){
     fs.unlinkSync(path.join(publicDir, "app.json"));
 }
 
-// Bumb patch number
-const patchNumber = parseInt(project.version.replace(/.*\./, "")) + 1;
-const newVersion = `${project.version.match(/.*\./)[0]}${patchNumber}`;
-data.build = newVersion;
-
-// Bumb version in package.json
-let package = fs.readFileSync(path.join(cwd, "package.json")).toString();
-package = package.replace(/\"version\"\:.*/g, `"version": "${newVersion}",`);
-fs.writeFileSync(path.join(cwd, "package.json"), package);
+// Set build version
+const data = {
+    build: project.version,
+};
 
 // Set build date
 const date = new Date();
